f323790e1057ea2293e23dd91d24204588d9dc9f
Santam Kumai <santtamk@gmail.com>
2024-07-25 00:02:36 +0530

    navbar is now responsive

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 
-import { useState } from 'react';
 import './App.css';
-import Main from './components/main/Main';
+// import Main from './components/main/Main';
 import SideNav from './components/sidenav/SideNav';
 
 function App() {
-  const [navBarOpen, setNavBarOpen] = useState(false);
+
   return (
-      <div className='bg-text01-gradient flex'>
-        <SideNav navBarOpen={navBarOpen} setNavBarOpen={setNavBarOpen} className="lg:w-1/5	"/>
-        <Main navBarOpen={navBarOpen} className="lg:w-4/5"/>
+      <div className='bg-text01-gradient'>
+        <SideNav  />
+        {/* <Main navBarOpen={navBarOpen}/> */}
       </div>
       
   );
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -3,17 +3,12 @@ import React from 'react'
 const Main = ({ navBarOpen }) => {
   return (
     <div className='flex flex-col'>
-        <div
-        className={`font-bold text-3xl text-primary ${
-          navBarOpen ? "hidden" : ""
-        } lg:block pl-4`}
-      >
-        Bot AI
         
-      </div>
         <div className='flex flex-col justify-center items-center gap-2 text-center'>
             <div>How Can I Help You Today?</div>
-            <div><img src='./botAi.png' alt='botAi' className='w-16 h-16 rounded-full shadow-2xl'/></div>
+            <div><img src='./botAi.png' alt='botAi' className='w-16 h-16 rounded-full drop-shadow-lg
+
+'/></div>
         </div>
     </div>
   )
