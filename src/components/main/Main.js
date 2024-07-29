d0948d9891565c1faa37c2e7f6a8f9502476824a
Santam Kumai <santtamk@gmail.com>
2024-07-29 13:53:05 +0530

    setting up a string function to get the answer from the sampleData.json

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,28 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import DATA from "../../sampleData.json";
+
+const DEMOQUESTIONS = [
+  {
+    id: "01",
+    question: "Hi, what is the weather?",
+  },
+  {
+    id: "02",
+    question: "Hi, what is my location?",
+  },
+  {
+    id: "03",
+    question: "Hi, what is the temperature?",
+  },
+  {
+    id: "04",
+    question: "Hi, how are you?",
+  },
+];
 
 const Main = () => {
-  const demoQuestions = [
-    {
-      id: "01",
-      question: "Hi, what is the weather?",
-    },
-    {
-      id: "02",
-      question: "Hi, what is my location?",
-    },
-    {
-      id: "03",
-      question: "Hi, what is the temperature?",
-    },
-    {
-      id: "04",
-      question: "Hi, how are you?",
-    },
-  ];
+  const [chatView, setChatView] = useState(false);
+  const [inputQuestion, setInputQuestion] = useState("");
+  const [showAnswer, setShowAnswer] = useState("");
+
+  // console.log("full data:", DATA);
+  // let input = "javascript?";
+
+  function string(words) {
+    console.log("words:", words.split(" "));
+    let findWord = DATA.find((data) =>
+      data.question.toLowerCase().includes(words.toLowerCase())
+    );
+    console.log("findWord:", findWord.question.split(" "));
+    if (findWord) {
+      console.log("found word:", findWord);
+      setShowAnswer(findWord.answer);
+    } else {
+      return "No related data found";
+    }
+  }
+  // console.log("found question:", string(input));
+  console.log(showAnswer);
 
   return (
     <div className="flex-1 lg:ml-1/5 p-4 transition-all duration-300 h-screen relative md:justify-center md:items-center  lg:mb-8 lg:gap-10 lg:flex lg:flex-col">
       {/* needs to be hidden when chat starts  */}
+      {/* chat section */}
       <div className="flex flex-col justify-center items-center gap-5 text-center pt-5 overflow-y-hidden">
         <div className="font-medium	text-3xl">How Can I Help You Today?</div>
         <div>
@@ -35,8 +60,9 @@ const Main = () => {
       </div>
 
       {/* suggestive questions */}
+
       <div className="flex flex-col gap-5 md:grid md:grid-rows-2 md:grid-cols-2 lg:grid lg:grid-cols-2 mt-5 snap-y	overscroll-contain">
-        {demoQuestions.map((questionItem, id) => (
+        {DEMOQUESTIONS.map((questionItem, id) => (
           <div
             className="bg-white	rounded-md shadow-lg p-4 cursor-pointer"
             key={questionItem.id}
@@ -48,13 +74,18 @@ const Main = () => {
           </div>
         ))}
       </div>
+      {/* chat section ends  */}
 
       <div className="fixed bottom-0 left-0 right-0 flex items-center justify-center gap-2 pb-2 ">
         <input
           type="text"
           className="w-64	h-10 rounded-md shadow-xl border border-b-text"
+          onChange={() => setInputQuestion(inputQuestion)}
         />
-        <button className="bg-secondary p-2 rounded-md text-base	font-normal shadow-xl">
+        <button
+          className="bg-secondary p-2 rounded-md text-base	font-normal shadow-xl"
+          onClick={() => string(inputQuestion)}
+        >
           Ask
         </button>
         <button className="bg-secondary p-2 text-base	rounded-md shadow-xl font-normal">
