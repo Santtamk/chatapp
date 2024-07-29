0941f0f06949d36014adb1c8c6dbbc9d59374e31
Santam Kumai <santtamk@gmail.com>
2024-07-29 22:20:47 +0530

    making changes to chat section

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -22,7 +22,7 @@ const DEMOQUESTIONS = [
 
 const Main = () => {
   const [chatView, setChatView] = useState(false);
-  const [inputQuestion, setInputQuestion] = useState("");
+  const [inputQuestion, setInputQuestion] = useState([]);
   const [showAnswer, setShowAnswer] = useState("");
 
   // console.log("full data:", DATA);
@@ -33,54 +33,86 @@ const Main = () => {
     let findWord = DATA.find((data) =>
       data.question.toLowerCase().includes(words.toLowerCase())
     );
-    console.log("findWord:", findWord.question.split(" "));
+    setChatView(true);
     if (findWord) {
-      console.log("found word:", findWord);
+      // console.log("findWord:", findWord.question.split(" "));
+      // console.log("found word:", findWord);
       setShowAnswer(findWord.answer);
     } else {
-      return "No related data found";
+      setShowAnswer("As an AI Language Model, I don’t have the details");
     }
   }
-  // console.log("found question:", string(input));
-  console.log(showAnswer);
+  // console.log(showAnswer);
 
   return (
-    <div className="flex-1 lg:ml-1/5 p-4 transition-all duration-300 h-screen relative md:justify-center md:items-center  lg:mb-8 lg:gap-10 lg:flex lg:flex-col">
+    <div
+      className={`flex-1 lg:ml-1/5 p-4 transition-all duration-300 h-screen relative md:justify-center md:items-center  lg:mb-8 lg:gap-10 lg:flex lg:flex-col ${
+        chatView ? "flex items-end" : ""
+      }`}
+    >
       {/* needs to be hidden when chat starts  */}
       {/* chat section */}
-      <div className="flex flex-col justify-center items-center gap-5 text-center pt-5 overflow-y-hidden">
-        <div className="font-medium	text-3xl">How Can I Help You Today?</div>
-        <div>
-          <img
-            src="./botAi.png"
-            alt="botAi"
-            className="w-16 h-16 rounded-full drop-shadow-lg"
-          />
-        </div>
-      </div>
-
-      {/* suggestive questions */}
-
-      <div className="flex flex-col gap-5 md:grid md:grid-rows-2 md:grid-cols-2 lg:grid lg:grid-cols-2 mt-5 snap-y	overscroll-contain">
-        {DEMOQUESTIONS.map((questionItem, id) => (
-          <div
-            className="bg-white	rounded-md shadow-lg p-4 cursor-pointer"
-            key={questionItem.id}
-          >
-            <div className="font-bold	text-xl	">{questionItem.question}</div>
-            <div className="text-base font-normal text-primary01">
-              Get immediate AI generated response
+      {!chatView ? (
+        <>
+          <div className="flex flex-col justify-center items-center gap-5 text-center pt-5 overflow-y-hidden">
+            <div className="font-medium	text-3xl">How Can I Help You Today?</div>
+            <div>
+              <img
+                src="./botAi.png"
+                alt="botAi"
+                className="w-16 h-16 rounded-full drop-shadow-lg"
+              />
             </div>
           </div>
-        ))}
-      </div>
+
+          {/* suggestive questions */}
+
+          <div className="flex flex-col gap-5 md:grid md:grid-rows-2 md:grid-cols-2 lg:grid lg:grid-cols-2 mt-5 snap-y	overscroll-contain">
+            {DEMOQUESTIONS.map((questionItem, id) => (
+              <div
+                className="bg-white	rounded-md shadow-lg p-4 cursor-pointer"
+                key={questionItem.id}
+              >
+                <div className="font-bold	text-xl	">{questionItem.question}</div>
+                <div className="text-base font-normal text-primary01">
+                  Get immediate AI generated response
+                </div>
+              </div>
+            ))}
+          </div>
+        </>
+      ) : (
+        <div className="flex flex-col items-end justify-end">
+          <div className="flex gap-5 text-center">
+            <div>
+              <img
+                src="./botAi.png"
+                alt="botAi"
+                className="w-16 h-16 rounded-full drop-shadow-lg"
+              />
+            </div>
+            <div className="font-medium  text-3xl">{inputQuestion}</div>
+          </div>
+          <div className="flex gap-5 text-center">
+            <div>
+              <img
+                src="./botAi.png"
+                alt="botAi"
+                className="w-16 h-16 rounded-full drop-shadow-lg"
+              />
+            </div>
+            <div className="font-medium  text-3xl">{showAnswer}</div>
+          </div>
+        </div>
+      )}
       {/* chat section ends  */}
 
       <div className="fixed bottom-0 left-0 right-0 flex items-center justify-center gap-2 pb-2 ">
         <input
           type="text"
           className="w-64	h-10 rounded-md shadow-xl border border-b-text"
-          onChange={() => setInputQuestion(inputQuestion)}
+          value={[...inputQuestion]}
+          onChange={(e) => setInputQuestion(e.target.value)}
         />
         <button
           className="bg-secondary p-2 rounded-md text-base	font-normal shadow-xl"
